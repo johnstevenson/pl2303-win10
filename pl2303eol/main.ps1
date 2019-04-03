5b50ac2588dd92a2e31f49d271c7ad1573adfb55
johnstevenson <john-stevenson@blueyonder.co.uk>
2019-04-03 18:44:50 +0100

    Add initial code and artefacts

---
+++ pl2303eol/main.ps1
@@ -0,0 +1,80 @@
+#Requires -Version 5.0 -RunAsAdministrator
+using module .\modules\PLApp.psm1
+
+$app = [PLApp]::new("$PSScriptRoot\driver")
+
+Write-Host 'Prolific PL-2303 USB-to-Serial driver. Compatible with unsupported'
+Write-Host 'end-of-life microchip versions (PL-2303HXA and PL-2303XA).'
+Write-Host "Driver version: $($app.Driver.Version) ($($app.Driver.Date))"
+Write-Host
+
+
+# *****************************************************************************
+# Show a list of PL-2303 drivers in the DriverStore then check if a PL-2303
+# driver installation program is installed.
+#
+# - If there is an installer, the user will be asked to uninstall it and the
+#   script will exit. This is because it could be uninstalled from the Control
+#   Panel later, which would remove our compatible driver.
+# *****************************************************************************
+$app.CheckForDrivers()
+$app.CheckForInstaller()
+
+
+# *****************************************************************************
+# Show the version of any installed driver and get confirmation to proceed.
+#
+# - The user can exit the script at this stage.
+# *****************************************************************************
+$app.GetConsent()
+
+
+# *****************************************************************************
+# Remove all PL-2303 drivers from the DriverStore then add our own driver.
+#
+# - Uses pnputil.exe for both operations. It is not uncommon for a removal to
+#   fail, if the DriverStore and internal driver database are out of sync. We
+#   let Windows sort this out the next time this script is run.
+# *****************************************************************************
+if (!($app.RemoveInstalledDrivers())) {
+    $msg = 'The following suggestions may help to resolve this issue:'
+    $app.IO.FinishWithHelp($msg)
+}
+
+if (!($app.InstallDriver())) {
+    # Something has gone badly wrong and there is not much we can do
+    $msg = 'Restarting your computer and running this script again may resolve the issue.'
+    $app.IO.Finish($msg, 1)
+}
+
+
+# *****************************************************************************
+# Although our driver is now the only PL2303 version in the DriverStore, the
+# sys file in the System drivers directory will not have been updated.
+#
+# - If the sys file is not the correct version, ask the user to re-plug in
+#   their device, so that Windows will recognize the new driver and copy the
+#   sys file from the DriverStore.
+# *****************************************************************************
+if ($app.CheckSystemSys()) {
+    # The correct sys file was already in the System directory
+    $app.IO.FinishWithInfo()
+}
+
+Write-Host
+Write-Host 'To get Windows to activate this driver, you must re-plug in your USB device.'
+$question = 'Enter Yes when you have done this, or No to skip this step'
+
+if (!($app.IO.PromptYes($question))) {
+    # The user chose to skip this step
+    $msg = 'If your USB device does not work, please try the following:'
+    $app.IO.FinishVerbose($msg)
+}
+
+if ($app.CheckSystemSys()) {
+    # The correct sys file is now in the System directory
+    $app.IO.FinishWithInfo()
+} else {
+    $msg = 'The new driver has not been activated. Please try the following:'
+    $app.IO.FinishVerbose($msg)
+}
---
+++ pl2303eol/modules/PLUtil.psm1
@@ -0,0 +1,98 @@
+class PLUtil
+{
+    static [bool] AddDriver([string]$infPath)
+    {
+        &pnputil.exe /add-driver """$infPath""" /install >$null 2>&1
+        return $LastExitCode -eq 0
+    }
+
+    static [bool] CheckSameVersion([string]$version1, [string]$version2)
+    {
+        if (!($version1) -or !($version2)) {
+            return $false
+        }
+
+        return [Version] $version1 -eq [Version] $version2
+    }
+
+    static [string] CreateTempDriverFolder([string]$driverPath)
+    {
+        $path = $Env:Temp
+        $folder = [Guid]::NewGuid().ToString()
+        $tempPath = Join-Path $path "{$folder}"
+
+        try {
+            Copy-Item $driverPath -Destination $tempPath -Recurse -ErrorAction SilentlyContinue
+        } catch {
+            $tempPath = [string]::Empty
+        }
+
+        return $tempPath
+    }
+
+    static [void] DeleteTempDriverFolder([string]$tempPath)
+    {
+        Get-ChildItem -Path $tempPath -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
+        Remove-Item $tempPath -Force -ErrorAction SilentlyContinue
+    }
+
+    static [array] GetDriverDateAndVersion([string]$data)
+    {
+        $date,$version = $data.Split('[, ]').Trim()
+        $parts = $date.Split('/')
+        $date = '{0}-{1}-{2}' -f $parts[1], $parts[0], $parts[2]
+
+        return @($date, $version)
+    }
+
+    static [string] GetFileVersion([string]$filePath)
+    {
+        if (!(Test-Path $filePath)) {
+            return [string]::Empty
+        }
+
+        $info = (Get-Item $filePath).VersionInfo
+
+        return ('{0}.{1}.{2}.{3}' -f $info.FileMajorPart,
+            $info.FileMinorPart,
+            $info.FileBuildPart,
+            $info.FilePrivatePart)
+    }
+
+    static [array] GetDrivers([string]$infName)
+    {
+        $drivers = @()
+        $publishedName = ''
+        $matched = $false
+        $pnpoutput = &pnputil.exe /enum-drivers
+
+        foreach ($line in $pnpoutput) {
+            $key,$value = $line.Split(':').Trim()
+
+            if ($key -eq 'Published Name') {
+                $publishedName = $value
+            } elseif ($key -eq 'Original Name') {
+                $matched = $value -eq $infName
+            } elseif ($key -eq 'Driver Version' -and $matched) {
+                $date, $version = [PLUtil]::GetDriverDateAndVersion($value)
+                $drivers += @{oem = $publishedName; date = $date; version = $version}
+                $matched = $false
+            }
+        }
+        return $drivers
+    }
+
+    static [array] RemoveDrivers([array]$drivers)
+    {
+        $errors = @()
+
+        foreach ($driver in $drivers) {
+            &pnputil.exe /delete-driver $driver.oem /uninstall /force >$null 2>&1
+
+            if ($LastExitCode -ne 0) {
+                $errors += $driver
+            }
+        }
+        return $errors
+    }
+}
